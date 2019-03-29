cac47e7d252337c93d97e3d6d3be0e49fad52833
V K <vinod.k.kumar@live.com>
2019-03-29 17:26:41 +0530

    Different operators in python

---
+++ Programs/Logical operators.py
@@ -0,0 +1,27 @@
+print("Logical operator")
+
+print("And operator")
+print("Is 13 >12 and 6<5 ? " + str(13 > 12 and 6 <5))
+print("Is 13 >12 and 5<6 ? " + str(13 > 12 and 5<6))
+
+print("Or operator")
+
+print("Is 13 >12 and 6<5 ? " + str(13 > 12 or 6 <5))
+print("Is 13 >12 and 6<5 ? " + str(13 > 12 or 5 <6))
+print("Is 13 <12 and 5<6 ? " + str(13 < 12 or 5 >6))
+
+print ("Not operator")
+a=6
+print ( not(a==5))
+
+a=5
+print ( not(a==5))s
+
+
+
+
+
+
+
+
+

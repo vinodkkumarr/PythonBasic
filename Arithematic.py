2b2a52ee0f21aecac0765181cefe06dbf9fb8558
V K <vinod.k.kumar@live.com>
2019-03-30 13:00:44 +0530

    First Commit

---
+++ Arithematic.py
@@ -0,0 +1,36 @@
+print("Python Mathematical Operations")
+print("Addition")
+a=12
+b=13
+print( "The addition of 12 and 13 is "+ str(a+b) )
+print("Subtraction")
+a=10
+b=5
+print("The subtraction of 10 and 5 is " + str(a-b))
+
+print("Multiplication")
+a=10
+b=5
+print("The multiplication of 10 and 5 is " + str(a*b))
+
+print("Multiplication")
+a=10
+b=5
+print("The multiplication of 10 and 5 is " + str(a*b))
+
+print("Division")
+a=13
+b=5
+print("The division of 10 and 5 is " + str(a/b))
+
+print("Floor Division")
+a=13
+b=5
+print("The whole number of divison of 13 and 5 is " + str(a//b))
+
+print ("Modulas operator")
+a=13
+b=5
+print("The remainder of the divison of 13 and 5 is "+ str(a%b))
+
+

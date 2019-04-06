acb5b5e26c87961e3cb7585f3255672074528398
V K <vinod.k.kumar@live.com>
2019-04-06 10:13:59 +0530

    For and If statement.

--- Programs/Membership Operators.py
+++
@@ -1,6 +0,0 @@
-print("membership operators (is and is not):It Works with Iterative operation so will work with String type only")
-x="Vinod Kumar"
-print("Is 'Vin' present in Vinod Kumar :" + str("Vin" in x))
-print("Is 'hello' present in Vinod Kumar :" + str( "Hello" in x))
-print("'Hello' is not present in Vinod Kumar :" + str("hello" not in x))
-
